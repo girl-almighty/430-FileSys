8f8090c006312651190a99a5db5360815e90645c
Hongfei Xie <daz657174868@gmail.com>
2018-06-01 17:02:56 -0700

    update filetableentry

--- ThreadOS/FileTableEntry.java
+++ ThreadOS/FileTableEntry.java
@@ -1,17 +1,21 @@
-public class FileTableEntry {  // Each table entry should have
-    public int seekPtr;        //    a file seek pointer
-    public final Inode inode;  //    a reference to an inode
-    public final short iNumber;//    this inode number
-    public int count;          //    a count to maintain #threads sharing this
-    public final String mode;  //    "r", "w", "w+", or "a"
-    FileTableEntry ( Inode i, short inumber, String m ) {
-	seekPtr = 0;           // the seek pointer is set to the file top.
-	inode = i;
-        iNumber = inumber;     
-        count = 1;           // at least one thread is using this entry.
-        mode = m;            // once file access mode is set, it never changes.
-
-	if ( mode.compareTo( "a" ) == 0 )
-	    seekPtr = inode.length;
-    }
+// This entry includes the seek pointer of this file, 
+// a reference to the inode corresponding to the file, the inode number, 
+// the count to maintain #threads sharing this file (structure) table, 
+// and the access mode. The seek pointer is set to the front or the tail 
+// of this file depending on the file access mode.
+public class FileTableEntry {          // Each table entry should have
+   public int seekPtr;                 //    a file seek pointer
+   public final Inode inode;           //    a reference to its inode
+   public final short iNumber;         //    this inode number
+   public int count;                   //    # threads sharing this entry
+   public final String mode;           //    "r", "w", "w+", or "a"
+   public FileTableEntry ( Inode i, short inumber, String m ) {
+      seekPtr = 0;             // the seek pointer is set to the file top
+      inode = i;
+      iNumber = inumber;
+      count = 1;               // at least on thread is using this entry
+      mode = m;                // once access mode is set, it never changes
+      if ( mode.compareTo( "a" ) == 0 ) // if mode is append,
+         seekPtr = inode.length;        // seekPtr points to the end of file
+   }
 }
